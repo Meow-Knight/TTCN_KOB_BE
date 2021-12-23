ca917c5ef0a34dfb2870135b8d2dcd3cc40986a6
huyviet2582000 <nguyencongvinh1412@gmail.com>
2021-12-23 21:42:14 +0700

    [FEAT] Create CRUD producer beer APIs

---
+++ api_admin/models/Producer.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from api_base.models import TimeStampedModel
+
+
+class Producer(TimeStampedModel):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "producer"
