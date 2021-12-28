5a4e86870a8b8b6ce2cb75b1a3b244d570a11139
huyviet2582000 <huyviet2582000@gmail.com>
2021-12-28 22:56:07 +0700

    [FIX] Edit detail beer info API

--- api_beer/serializers/Beer.py
+++ api_beer/serializers/Beer.py
@@ -1,4 +1,7 @@
+import datetime
+
 from rest_framework import serializers
+from api_beer.models import Beer, BeerPhoto, BeerDiscount
 
 from api_beer.models import Beer, BeerPhoto
 from django.db import models
@@ -33,8 +36,30 @@ class RetrieveBeerSerializer(serializers.ModelSerializer):
         return data
 
 
-class BeerDetailSerializer(serializers.ModelSerializer):
+class ItemBeerSerializer(serializers.ModelSerializer):
+    """
+    This serializer contains beer record include extend fields:
+    - First photo
+    - Available sale discount
+    """
+
+    def to_representation(self, instance):
+        data = super(ItemBeerSerializer, self).to_representation(instance)
+        beer_id = data['id']
+        photos = BeerPhoto.objects.filter(beer_id=beer_id)
+        if photos.exists():
+            data['photo'] = photos.first().link
+        discount = BeerDiscount.objects.filter(beer_id=beer_id,
+                                               discount__start_date__lte=datetime.date.today(),
+                                               discount__end_date__gte=datetime.date.today(),
+                                               discount__is_activate=True).values("discount_percent")
+        if discount.exists():
+            discount = discount.first()
+            data.update(discount)
+
+        return data
+
     class Meta:
         model = Beer
-        fields = '__all__'
+        fields = ('id', 'name', 'capacity', 'price',)
         depth = 1
--- api_beer/views/Beer.py
+++ api_beer/views/Beer.py
@@ -1,12 +1,10 @@
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
-from api_beer.serializers import BeerSerializer, ListBeerSerializer, BeerPhotoSerializer
-from api_beer.models import Beer, BeerPhoto
-from api_beer.serializers.Beer import BeerDetailSerializer
-from api_beer.serializers import BeerSerializer, ListBeerSerializer, RetrieveBeerSerializer
+from api_beer.models import BeerPhoto
+from api_beer.serializers import BeerSerializer, ListBeerSerializer, RetrieveBeerSerializer, ItemBeerSerializer, BeerPhotoSerializer
 from api_beer.models import Beer
 from api_beer.services import BeerService
 from api_base.views import BaseViewSet
@@ -54,14 +52,14 @@ class BeerViewSet(BaseViewSet):
 
     @action(detail=True, methods=['get'])
     def info(self, request, pk, *args, **kwargs):
-        beer = Beer.objects.get(pk=pk)
-        photo = BeerPhoto.objects.filter(beer=beer.id)
-        beer_producer = Beer.objects.filter(producer=beer.producer)
-        if beer:
-            beer = BeerDetailSerializer(beer)
-            beer_producer = ListBeerSerializer(beer_producer, many=True)
-            photo = BeerPhotoSerializer(photo.first())
-            return Response({"detail": beer.data, "photo": photo.data, "beer_nation": beer_producer.data},
-                            status=status.HTTP_200_OK)
-        else:
-            return Response({"details": "Cannot get beer detail information"}, status=status.HTTP_400_BAD_REQUEST)
+        beer = self.get_object()
+        photos = BeerPhoto.objects.filter(beer=beer.id).values('link')
+        same_producer_beers = Beer.objects.filter(producer=beer.producer).exclude(id=beer.id)
+        beer = ListBeerSerializer(beer)
+        beer_producer_serializer = ItemBeerSerializer(same_producer_beers, many=True)
+        res_data = {"details": beer.data}
+        if photos.exists():
+            res_data['photos'] = map(lambda photo: photo['link'], list(photos))
+        if same_producer_beers.exists():
+            res_data['same_producer_beers'] = beer_producer_serializer.data
+        return Response(res_data, status=status.HTTP_200_OK)
