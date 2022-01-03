3f6333c4bc4f22e9fbcf1158237f6a557884314c
huyviet2582000 <huyviet2582000@gmail.com>
2022-01-03 16:27:33 +0700

    [FEAT] Add profile fields into Account model

--- api_account/models/Account.py
+++ api_account/models/Account.py
@@ -13,6 +13,9 @@ class Account(AbstractUser):
     avatar = models.CharField(max_length=200, null=True, blank=True)
     role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
     is_active = models.BooleanField(default=True)
+    address = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
 
     USERNAME_FIELD = 'username'
 
