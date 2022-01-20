9308fab3c4a5e33b7b9be05e799740feba036714
huyviet2582000 <huyviet2582000@gmail.com>
2022-01-20 20:24:21 +0700

    [FIX] can_create_review API

--- api_account/services/Review.py
+++ api_account/services/Review.py
@@ -1,3 +1,4 @@
+from api_account.models import Review
 from api_order.models import OrderDetail, OrderStatus
 from api_order import constants
 
@@ -6,4 +7,6 @@ class ReviewService:
     @classmethod
     def can_create_review(cls, account, beer):
         done_status = OrderStatus.objects.get(id=constants.OrderStatus.COMPLETED.value.get('id'))
-        return OrderDetail.objects.filter(beer=beer, order__account=account, order__order_status=done_status).exists()
+        if OrderDetail.objects.filter(beer=beer, order__account=account, order__order_status=done_status).exists():
+            return not Review.objects.filter(account=account, beer=beer).exists()
+        return False
