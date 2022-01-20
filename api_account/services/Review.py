8fe72f38f0cec47ed042f338094724d8c376f0ee
huyviet2582000 <huyviet2582000@gmail.com>
2022-01-20 19:25:31 +0700

    [FIX] change OrderStatus enum in can_create_review API

--- api_account/services/Review.py
+++ api_account/services/Review.py
@@ -5,5 +5,5 @@ from api_order import constants
 class ReviewService:
     @classmethod
     def can_create_review(cls, account, beer):
-        done_status = OrderStatus.objects.get(name=constants.OrderStatus.DONE.name)
+        done_status = OrderStatus.objects.get(id=constants.OrderStatus.COMPLETED.value.get('id'))
         return OrderDetail.objects.filter(beer=beer, order__account=account, order__order_status=done_status).exists()
